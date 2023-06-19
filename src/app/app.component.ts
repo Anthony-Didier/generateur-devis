7b50a4feb92e95648dae582f0f52b2b86225e434
Anthony-Didier <didieranthony1@gmail.com>
2023-06-19 18:25:57 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,293 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'generateur_devis';
+
+  public downloadInvoice() {
+
+    const date = new Date();
+
+    let day = date.getDate();
+    let month = ("0" + (date.getMonth() + 1)).slice(-2)
+    let year = date.getFullYear();
+
+    let currentDate = `${day}/${month}/${year}`;
+
+    let prestDate = new Date((<HTMLInputElement>document.getElementById("date")).value).toLocaleDateString();
+    let number = (<HTMLInputElement>document.getElementById("number")).value;
+    let place = (<HTMLInputElement>document.getElementById("place")).value;
+    let time = (<HTMLInputElement>document.getElementById("time")).value;
+    let prestation = (<HTMLInputElement>document.getElementById("prestation")).value;
+    let name = (<HTMLInputElement>document.getElementById("name")).value;
+    let service = (<HTMLInputElement>document.getElementById("service")).value;
+    let po = (<HTMLInputElement>document.getElementById("po")).value;
+
+    var doc = new jsPDF();
+    let img = new Image();
+    img.src = "assets/eurest.png"
+
+    doc.addImage(
+      img,
+      'PNG',
+      (doc.internal.pageSize.width - 40) / 2,
+      5,
+      40,
+      28
+    );
+
+    // autoTable(doc, {
+    //   body: [
+    //     [
+    //       {
+    //         content: 'Company brand',
+    //         styles: {
+    //           halign: 'left',
+    //           fontSize: 20,
+    //           textColor: '#ffffff'
+    //         }
+    //       },
+    //       {
+    //         content: 'Invoice',
+    //         styles: {
+    //           halign: 'right',
+    //           fontSize: 20,
+    //           textColor: '#ffffff'
+    //         }
+    //       }
+    //     ],
+    //   ],
+    //   theme: 'plain',
+    //   styles: {
+    //     fillColor: '#3366ff'
+    //   }
+    // });
+
+    autoTable(doc, {
+      body: [
+        [
+          {
+            content: 'Devis effectué le :',
+            styles: {
+              halign: 'left',
+              cellWidth: 73
+            }
+          },
+          {
+            content: currentDate,
+            styles: {
+              halign: 'left',
+              fontStyle: 'bold'
+            },
+          }
+        ],
+      ],
+      theme: 'plain',
+      margin: { top: 40 }
+    });
+
+    // autoTable(doc, {
+    //   body: [
+    //     [
+    //       {
+    //         content: 'Billed to:'
+    //           + '\nJohn Doe'
+    //           + '\nBilling Address line 1'
+    //           + '\nBilling Address line 2'
+    //           + '\nZip code - City'
+    //           + '\nCountry',
+    //         styles: {
+    //           halign: 'left'
+    //         }
+    //       },
+    //       {
+    //         content: 'Shipping address:'
+    //           + '\nJohn Doe'
+    //           + '\nShipping Address line 1'
+    //           + '\nShipping Address line 2'
+    //           + '\nZip code - City'
+    //           + '\nCountry',
+    //         styles: {
+    //           halign: 'left'
+    //         }
+    //       },
+    //       {
+    //         content: 'From:'
+    //           + '\nCompany name'
+    //           + '\nShipping Address line 1'
+    //           + '\nShipping Address line 2'
+    //           + '\nZip code - City'
+    //           + '\nCountry',
+    //         styles: {
+    //           halign: 'right'
+    //         }
+    //       }
+    //     ],
+    //   ],
+    //   theme: 'plain'
+    // });
+
+    autoTable(doc, {
+      body: [
+        [
+          {
+            content: 'Date de la prestation :'
+              + '\nNombre de convives :'
+              + '\nLieu :'
+              + '\nHeure :'
+              + '\nPrestation :',
+            styles: {
+              halign: 'left',
+              fontStyle: 'bold',
+              cellWidth: 73
+            }
+          },
+          {
+            content: prestDate
+              + '\n' + number
+              + '\n' + place
+              + '\n' + time
+              + '\n' + prestation,
+            styles: {
+              halign: 'left'
+            }
+          }
+        ],
+      ],
+      theme: 'plain'
+    });
+
+    autoTable(doc, {
+      head: [['Désignation', 'Commentaires', 'Prix unitaire HT', 'Quantité', 'TVA', 'Total HT']],
+      body: [
+        ['Product or service name', 'Category', '$450', '2', '10 %', '$1000'],
+        ['Product or service name', 'Category', '$450', '2', '10 %', '$1000'],
+        ['Product or service name', 'Category', '$450', '2', '10 %', '$1000'],
+        ['Product or service name', 'Category', '$450', '2', '20 %', '$1000']
+      ],
+      theme: 'striped',
+      headStyles: {
+        fillColor: '#343a40'
+      }
+    });
+
+    autoTable(doc, {
+      body: [
+        [
+          {
+            content: 'Subtotal:',
+            styles: {
+              halign: 'right'
+            }
+          },
+          {
+            content: '$3600',
+            styles: {
+              halign: 'right'
+            }
+          },
+        ],
+        [
+          {
+            content: 'Total tax:',
+            styles: {
+              halign: 'right'
+            }
+          },
+          {
+            content: '$400',
+            styles: {
+              halign: 'right'
+            }
+          },
+        ],
+        [
+          {
+            content: 'Total amount:',
+            styles: {
+              halign: 'right'
+            }
+          },
+          {
+            content: '$4000',
+            styles: {
+              halign: 'right'
+            }
+          },
+        ],
+      ],
+      theme: 'plain'
+    });
+
+    autoTable(doc, {
+      body: [[
+        {
+          content: 'Facturation',
+          styles: {
+            halign: 'left',
+            fontStyle: 'bold',
+            fillColor: '#343a40',
+            textColor: 'white',
+          },
+          colSpan: 2
+        },
+        {
+          content: '',
+          styles: {
+            halign: 'left'
+          }
+        }
+      ],
+      [
+        {
+          content: 'Nom :'
+            + '\nService :'
+            + '\nCode facturation (P.O) :',
+          styles: {
+            halign: 'left',
+            fontStyle: 'bold',
+            cellWidth: 73
+          }
+        },
+        {
+          content: name
+            + '\n' + service
+            + '\n' + po,
+          styles: {
+            halign: 'left',
+            cellWidth: (doc.internal.pageSize.width - 73) - 28
+          }
+        }
+      ],
+      ],
+      theme: 'plain'
+    });
+
+    // autoTable(doc, {
+    //   body: [
+    //     [
+    //       {
+    //         content: 'Commande effectuée par :',
+    //         styles: {
+    //           halign: 'left',
+    //           fontStyle: 'bold',
+    //           cellWidth: 73
+    //         }
+    //       },
+    //       {
+    //         content: 'Alain DIDIER',
+    //         styles: {
+    //           halign: 'left'
+    //         }
+    //       }
+    //     ],
+    //   ],
+    //   theme: 'plain'
+    // });
+
+    return doc.save("invoice");
+
+  }
+
+  ngOnInit(): void {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
